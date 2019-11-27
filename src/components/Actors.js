c0e6aad3fa351776fdfc85e238b3c85b68642d8b
Graham Flaspoehler <grahamflas@gmail.com>
2019-11-27 06:24:04 -0500

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,9 +4,22 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {
+        actors.map( actor => {
+          return(
+            <div key={actor.name}>
+              <h3>{actor.name}</h3>
+              {
+                actor.movies.map( movie => <li key={movie}>{movie}</li> )
+              }
+            </div>
+          )
+        } )
+      }
+
     </div>
   );
-};
+}
 
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import { movies } from '../data';
 
+console.log(movies)
+
 const Movies = () => {
-  return (
+  return(
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {
+        movies.map( movie => {
+          return (
+            <div key={movie.title}>
+              <h3>{movie.title}</h3>
+              <p>Time: {movie.time}</p>
+              <ul>
+                {
+                  movie.genres.map( genre => <li>{genre}</li> )
+                }
+              </ul>
+            </div>
+          )
+        } )
+      }
     </div>
-  );
+  )
 };
 
 export default Movies;
